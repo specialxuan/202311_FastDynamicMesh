c01c25b00e1c3a11eebedf1901fcc4ec1c2768a4
SpecialXuan <special.xuan@outlook.com>
2023-11-22 16:21:56 +0800

    update comments

--- FastDynamicMesh.c
+++ FastDynamicMesh.c
@@ -1,28 +1,28 @@
-/* ************************************************
-			 Dynamic mesh UDF edited by LKD-2023
-	   fluid structure algorithm proposed By LKD & WZR
-**************************************************/
+// 			 Dynamic mesh UDF edited by LKD-2023
+// 	   fluid structure algorithm proposed By LKD & WZR
+//          Refactored by SpecialXuan at 2023.11
+
 #include "udf.h"
-/*********************************************
-				define 1 array variables:
- elas_mode are the first 4 mode shapes array of the fluid mesh.
- node-No.,x-coor,y-coor,z-coor,shape-information
-*************************************************/
+
+// 				   define 1 array variables:
+//  elas_mode are the first 4 mode shapes array of the fluid mesh.
+//       node-No.,x-coor,y-coor,z-coor,shape-information
 static double elas_mode[66654][16];
-/* 66654 row, 16column, Start at 0 when called*/
-/* define the read_mode()  to read input the array from the .txt file */
+// 66654 row, 16column, Start at 0 when called*/
+
+// define the read_mode()  to read input the array from the .txt file
 void read_my_mode()
 {
-	/* define variables, file pointer */
+	// define variables, file pointer
 	FILE *fp;
 	int i, j, k;
-	/* open the file in the read-only mode */
+	// open the file in the read-only mode
 	fp = fopen("elas_mode.txt", "r");
 	if (fp == NULL)
 		Message("Error, Can not find the file.\n");
 	else
 	{
-		/* initialise the array */
+		// initialise the array
 		for (i = 0; i < 66654; i++)
 		{
 			for (j = 0; j < 16; j++)
@@ -32,7 +32,7 @@ void read_my_mode()
 		}
 	}
 	fclose(fp);
-	/* validation for the initialization of the array: print out the variables */
+	// validation for the initialization of the array: print out the variables
 	Message("\n*******************************************\n");
 	Message("\n The elas_mode array is:\n");
 	for (k = 0; k < 16; k++)
@@ -45,9 +45,9 @@ void read_my_mode()
 	Message("\n Validation is done! \n");
 	Message("\n*******************************************\n");
 }
-static int count = 0;	   /* record the number of total nodes */
-static int iter_index = 1; /* record the number of the iteration steps */
-static int time_index = 0; /* record the number of the time steps */
+static int count = 0;	   // record the number of total nodes
+static int iter_index = 1; // record the number of the iteration steps
+static int time_index = 0; // record the number of the time steps
 DEFINE_ON_DEMAND(Preparation)
 {
 	Domain *domain;
@@ -58,89 +58,89 @@ DEFINE_ON_DEMAND(Preparation)
 	count = 0;
 	iter_index = 1;
 	time_index = 0;
-	/******************************************************
-				   set the flow time
-	RP_Get_Real(char *s)-RP_Get_Real("flow-time")
-	RP_Set_Float(char *s, double v)-RP_Set_Float("flow-time", 0.2)
-	*******************************************************/
+
+	// 			        set  flow time
+	// RP_Get_Real(char *s)-RP_Get_Real("flow-time")
+	// RP_Set_Float(char *s, double v)-RP_Set_Float("flow-time", 0.2)
 	RP_Set_Float("flow-time", 0);
-	/* for single-phase flows, domain_id is 1 and Get_Domain(1) returns the fluid domain pointer */
+	
+	// for single-phase flows, domain_id is 1 and Get_Domain(1) returns the fluid domain pointer
 	domain = Get_Domain(1);
 	read_my_mode();
-	/* Store the modal shape information in user-defined node memory */
+	
+	// Store the modal shape information in user-defined node memory
 	thread_loop_c(t, domain){
 		begin_c_loop(c, t){
 			c_node_loop(c, t, n){
 				v = C_NODE(c, t, n);
-	/* set the value of the first user define node memory as 100 */
-	/* used to identify if the node has stored the shape information or not */
-	N_UDMI(v, 0) = 100;
-}
-}
-end_c_loop(c, t)
-}
-thread_loop_c(t, domain){
+				
+				// set the value of the first user define node memory as 100
+				// used to identify if the node has stored the shape information or not
+				N_UDMI(v, 0) = 100;
+			}
+		}
+		end_c_loop(c, t)
+	}
+	thread_loop_c(t, domain){
 	begin_c_loop(c, t){
 		c_node_loop(c, t, n){
 			v = C_NODE(c, t, n);
-/* hold the modal shape information using the UDMI */
-if (N_UDMI(v, 0) == 100)
-{
-	for (i = 0; i < 12; i++)
-	{
-		N_UDMI(v, i) = elas_mode[count][i + 4];
-	}
-	count += 1;
-}
-}
-}
-end_c_loop(c, t)
-}
-Message("\n*******************************************\n");
-Message("\n The number of total nodes is: \n");
-Message("%d \t", count);
-Message("\n*******************************************\n");
+				// hold the modal shape information using the UDMI
+				if (N_UDMI(v, 0) == 100)
+				{
+					for (i = 0; i < 12; i++)
+					{
+						N_UDMI(v, i) = elas_mode[count][i + 4];
+					}
+					count += 1;
+				}
+			}
+		}
+		end_c_loop(c, t)
+	}	
+	Message("\n*******************************************\n");
+	Message("\n The number of total nodes is: \n");
+	Message("%d \t", count);
+	Message("\n*******************************************\n");
 }
 
-/********************************************
-calculation of the modal aerodynamic force
-and modal displacement for fluid mesh
-Node motion for CFD mesh of the fluid zone
-********************************************/
-static real m = 1; /* modal mass = 1, which means normalization of principal mass */
-static real c = 0; /* modal damping */
-static real Pi = 3.141592654;
-static real theta = 1.4;								/* wilson-theta method, the value of the theta */
-static int FSI_ID = 19;									/* record the id of the fsi faces, shown in fluent */
-static int Fluid_ID = 11;								/* record the id of the fluid cell zone, shown in fluent */
-static real freq[4] = {0.17839, 1.103, 3.0322, 5.8042}; /* frequencies of the structure */
-static real ini_vel[4] = {0, 0, 0, 0};					/* initial modal velocity */
-static real mode_force[100000][5] = {0};				/* modal force */
-static real mode_disp[100000][12] = {0};				/* modal-displacement,modal-velocity,acceleration repeat */
+// calculation of the modal aerodynamic force
+// and modal displacement for fluid mesh
+// Node motion for CFD mesh of the fluid zone
+static real m = 1;										// modal mass = 1, which means normalization of principal mass
+static real c = 0;										// modal damping
+static real Pi = 3.141592654;							// Pi
+static real theta = 1.4;								// wilson-theta method, the value of the theta
+static int FSI_ID = 19;									// record the id of the fsi faces, shown in fluent
+static int Fluid_ID = 11;								// record the id of the fluid cell zone, shown in fluent
+static real freq[4] = {0.17839, 1.103, 3.0322, 5.8042}; // frequencies of the structure
+static real ini_vel[4] = {0, 0, 0, 0};					// initial modal velocity
+static real mode_force[100000][5] = {0};				// modal force
+static real mode_disp[100000][12] = {0};				// modal-displacement,modal-velocity,acceleration repeat
 DEFINE_GRID_MOTION(FDM_method, domain, dt, time, dtime)
 {
-	Thread *t_fsi = Lookup_Thread(domain, FSI_ID); /* pointer to the thread of FSI faces */
-	Thread *t = Lookup_Thread(domain, Fluid_ID);   /* pointer to a thread */
-	real N_nodes, Pres, Area[3];
-	real disp[3] = {0}; /* record the displacement of the cfd mesh node */
-	cell_t c;			/* An integer data type that identifies a particular cell within a cell thread. */
-	face_t f;			/* An integer data type that identifies a particular face within a face thread. */
-	Node *node;			/* A structure data type that stores data associated with a mesh point */
+	Thread *t_fsi = Lookup_Thread(domain, FSI_ID); // pointer to the thread of FSI faces
+	Thread *t = Lookup_Thread(domain, Fluid_ID);   // pointer to a thread
+	real N_nodes, Pres, Area[3];				   // number of nodes in a face, pressure, normal vector of a face
+	real disp[3] = {0};							   // record the displacement of the cfd mesh node
+	cell_t c;									   // An integer data type that identifies a particular cell within a cell thread.
+	face_t f;									   // An integer data type that identifies a particular face within a face thread.
+	Node *node;									   // A structure data type that stores data associated with a mesh point
 	int node_index, i;
 	for (i = 1; i < 5; i++)
 	{
 		mode_force[time_index][i] = 0;
 	}
-	begin_f_loop(f, t_fsi) /* loop over all faces in a given face thread. */
+	begin_f_loop(f, t_fsi) // loop over all faces in a given face thread.
 	{
-		F_AREA(Area, f, t_fsi); /* (A,f,t):A[ND_ND], face_t f, Thread *t, A (area vector) */
+		F_AREA(Area, f, t_fsi); //(A,f,t):A[ND_ND], face_t f, Thread *t, A (area vector)
 
-		Pres = F_P(f, t_fsi);			  /* (f,t): face_t f, Thread *t, pressure */
-		N_nodes = F_NNODES(f, t_fsi);	  /* (f,t), face_t f, Thread *t, number of nodes in a face */
-		f_node_loop(f, t_fsi, node_index) /* loops over all nodes of a given face */
+		Pres = F_P(f, t_fsi);			  //(f,t): face_t f, Thread *t, pressure
+		N_nodes = F_NNODES(f, t_fsi);	  //(f,t), face_t f, Thread *t, number of nodes in a face
+		f_node_loop(f, t_fsi, node_index) // loops over all nodes of a given face
 		{
-			node = F_NODE(f, t_fsi, node_index); /* the F_NODE macro to obtain the global face node number */
-			for (i = 1; i < 5; i++)				 /* calculation of the modal aerodynamic force */
+			node = F_NODE(f, t_fsi, node_index); // the F_NODE macro to obtain the global face node number
+			for (i = 1; i < 5; i++)				 // calculation of the modal aerodynamic force
 			{
 				mode_force[time_index][i] += 1 / N_nodes * Pres * (Area[0] * N_UDMI(node, 3 * i - 3) + Area[1] * N_UDMI(node, 3 * i - 2) + Area[2] * N_UDMI(node, 3 * i - 1));
 			}
@@ -150,9 +150,7 @@ DEFINE_GRID_MOTION(FDM_method, domain, dt, time, dtime)
 		Message("(");
 	Message("%d", iter_index);
 	Message(")");
-	/****************************************************
-   calculation of modal displacement and modal velocity
-   *****************************************************/
+	//    calculation of modal displacement and modal velocity
 	if (time_index == 0)
 	{
 		for (i = 1; i < 5; i++)
@@ -167,16 +165,16 @@ DEFINE_GRID_MOTION(FDM_method, domain, dt, time, dtime)
 	{
 		for (i = 0; i < 4; i++)
 		{
-			/* the calculation of modal displacement,  acceleration, velocity using wilson-theta method */
+			// the calculation of modal displacement,  acceleration, velocity using wilson-theta method
 			mode_disp[time_index][3 * i] = mode_force[time_index - 1][i + 1] + theta * (mode_force[time_index][i + 1] - mode_force[time_index - 1][i + 1]);
 			mode_disp[time_index][3 * i] += m * (6 / SQR(theta * dtime) * mode_disp[time_index - 1][3 * i] + 6 / (theta * dtime) * mode_disp[time_index - 1][3 * i + 1] + 2 * mode_disp[time_index - 1][3 * i + 2]);
 			mode_disp[time_index][3 * i] += c * (3 / (theta * dtime) * mode_disp[time_index - 1][3 * i] + 2 * mode_disp[time_index - 1][3 * i + 1] + 0.5 * theta * dtime * mode_disp[time_index - 1][3 * i + 2]);
 			mode_disp[time_index][3 * i] /= 6 * m / SQR(theta * dtime) + 3 * c / (theta * dtime) + SQR(2 * Pi * freq[i]);
-			/* calculation of the acceleration */
+			// calculation of the acceleration
 			mode_disp[time_index][3 * i + 2] = 6 / (SQR(theta * dtime) * theta) * (mode_disp[time_index][3 * i] - mode_disp[time_index - 1][3 * i]) - 6 / (SQR(theta) * dtime) * mode_disp[time_index - 1][3 * i + 1] + (1 - 3 / theta) * mode_disp[time_index - 1][3 * i + 2];
-			/* calculation of the velocity */
+			// calculation of the velocity
 			mode_disp[time_index][3 * i + 1] = mode_disp[time_index - 1][3 * i + 1] + 0.5 * dtime * (mode_disp[time_index][3 * i + 2] + mode_disp[time_index - 1][3 * i + 2]);
-			/* calculation of the displacement */
+			// calculation of the displacement
 			mode_disp[time_index][3 * i] = mode_disp[time_index - 1][3 * i] + dtime * mode_disp[time_index - 1][3 * i + 1] + SQR(dtime) / 6 * (mode_disp[time_index][3 * i + 2] + 2 * mode_disp[time_index - 1][3 * i + 2]);
 		}
 	}
@@ -189,14 +187,14 @@ DEFINE_GRID_MOTION(FDM_method, domain, dt, time, dtime)
 				node = C_NODE(c, t, node_index);
 				if (NODE_POS_NEED_UPDATE(node))
 				{
-					/* set the value of disp[3] with zeros to calculate the displacement for the current time step */
+					// set the value of disp[3] with zeros to calculate the displacement for the current time step
 					for (i = 0; i < 3; i++)
 					{
 						disp[i] = 0;
 					}
-					/* indicate that the position of the node has been updated to avoid updating more than once */
+					// indicate that the position of the node has been updated to avoid updating more than once
 					NODE_POS_UPDATED(node);
-					/* calculation of nodal displacement for the current node */
+					// calculation of nodal displacement for the current node
 					for (i = 0; i < 4; i++)
 					{
 						disp[0] += (mode_disp[time_index][3 * i] - mode_disp[time_index - 1][3 * i]) * N_UDMI(node, 3 * i);
@@ -211,9 +209,8 @@ DEFINE_GRID_MOTION(FDM_method, domain, dt, time, dtime)
 	}
 	iter_index += 1; /**increase the total number of iteration steps**/
 }
-/****************************************************
-keep the flow_time information and increase the number of time steps
-*****************************************************/
+
+// keep the flow_time information and increase the number of time steps
 DEFINE_EXECUTE_AT_END(setting_next_time_step)
 {
 	int i;
@@ -230,14 +227,13 @@ DEFINE_EXECUTE_AT_END(setting_next_time_step)
 		Message("%12.10e\t", mode_disp[time_index][i]);
 	}
 	Message("\n**************************************\n");
-	iter_index = 1;							  /* set the value of the iterations number to be 1 */
-	time_index += 1;						  /**increase the total number of time steps, used as the index for mode_force and mode_disp**/
-	mode_force[time_index][0] = CURRENT_TIME; /**record the flow time at the current time step**/
+	iter_index = 1;							  // set the value of the iterations number to be 1
+	time_index += 1;						  // increase the total number of time steps, used as the index for mode_force and mode_disp
+	mode_force[time_index][0] = CURRENT_TIME; // record the flow time at the current time step
 }
-/*************************************************
-define_on_demand
-to output the calculated modal force, displacement and velocity to a txt file
-*************************************************/
+
+// define_on_demand
+// to output the calculated modal force, displacement and velocity to a txt file
 DEFINE_ON_DEMAND(Output_disp)
 {
 	FILE *fp;
