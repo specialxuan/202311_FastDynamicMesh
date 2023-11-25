37f2d8462513c84baa9ef7a6dd821cc125071ccd
SpecialXuan <special.xuan@outlook.com>
2023-11-25 14:24:08 +0800

    update row column

--- FastDynamicMesh.c
+++ FastDynamicMesh.c
@@ -5,10 +5,10 @@
 #include "udf.h"
 
 // define the read_mode()  to read input the array from the .txt file
-int read_my_mode(double *elas_mode)
+int read_my_mode(double *elas_mode, const int row, const int column)
 {
-	FILE *fp; // file pointer
-	int i, j; // indexes
+	FILE *fp = NULL; // file pointer
+	int i = 0, j = 0; // indexes
 
 	fp = fopen("elas_mode.txt", "r"); // open the file in the read-only mode
 	if (fp == NULL)					  // file not exists -> print error
@@ -17,9 +17,9 @@ int read_my_mode(double *elas_mode)
 		return 1;
 	}
 
-	for (i = 0; i < 66654; i++) // fill the array
-		for (j = 0; j < 16; j++)
-			if (fscanf(fp, "%lf", elas_mode + i * 16 + j) <= 0)
+	for (i = 0; i < row; i++) // fill the array
+		for (j = 0; j < column; j++)
+			if (fscanf(fp, "%lf", elas_mode + i * column + j) <= 0)
 			{
 				Message("\n ---   Error: Lack of variables.   ---\n From (%d,%d)", i, j);
 				fclose(fp);
@@ -29,11 +29,11 @@ int read_my_mode(double *elas_mode)
 	fclose(fp); // close file
 
 	Message("\n ---    The elas_mode array is:    ---\n"); // validation for the initialization of the elas_mode
-	for (j = 0; j < 16; j++)						 // print variables
+	for (j = 0; j < column; j++)						 // print variables
 		if (j == 0)
-			Message("%f \t", elas_mode[66654 * 16 + j]);
+			Message("%f \t", elas_mode[row * column + j]);
 		else
-			Message("%6.5e \t", elas_mode[66654 * 16 + j]);
+			Message("%6.5e \t", elas_mode[row * column + j]);
 	Message("\n ---      Validation is done!      ---\n");
 
 	return 0;
@@ -44,18 +44,18 @@ static int time_index = 0; // record the number of the time steps
 
 DEFINE_ON_DEMAND(Preparation)
 {
-	FILE *fp;
+	FILE *fp = NULL;
 
 	Domain *domain;
 	cell_t c;
 	Thread *t;
 	Node *v;
-	int n, i, count;   // record the number of total nodes
-	double *elas_mode; // elas_mode are the first 4 mode shapes array of the fluid mesh.
+	int row = 66654, column = 16;
+	int n = 0, i = 0, count = 0;   // record the number of total nodes
+	double *elas_mode = NULL; // elas_mode are the first 4 mode shapes array of the fluid mesh.
 					   // node-No.,x-coor,y-coor,z-coor,shape-information
 					   // 66654 row, 16column, Start at 0 when called
 
-	count = 0;
 	iter_index = 1;
 	time_index = 0;
 
@@ -64,10 +64,10 @@ DEFINE_ON_DEMAND(Preparation)
 	RP_Set_Float("flow-time", 0); // set  flow time
 
 	domain = Get_Domain(1);									   // for single-phase flows, domain_id is 1 and Get_Domain(1) returns the fluid domain pointer
-	elas_mode = (double *)malloc(66654 * 16 * sizeof(double)); // allocate memory for elas_mode
-	memset(elas_mode, 0, 66654 * 16 * sizeof(double));		   // initialize elas_mode
+	elas_mode = (double *)malloc(row * column * sizeof(double)); // allocate memory for elas_mode
+	memset(elas_mode, 0, row * column * sizeof(double));		   // initialize elas_mode
 
-	if (read_my_mode(elas_mode))
+	if (read_my_mode(elas_mode, row, column))
 		Message("\n ---      Error: read_my_mode      ---\n");
 	else
 	{
@@ -97,7 +97,7 @@ DEFINE_ON_DEMAND(Preparation)
 					if (N_UDMI(v, 12) == 0) // hold the modal shape information using the UDMI
 					{
 						for (i = 0; i < 12; i++)
-							N_UDMI(v, i) = elas_mode[count * 16 + i + 1];
+							N_UDMI(v, i) = elas_mode[count * column + i + 1];
 					
 						fprintf(fp, "%5d, %5d,", v, count);
 						count++;
@@ -113,7 +113,7 @@ DEFINE_ON_DEMAND(Preparation)
 		fclose(fp);
 
 		Message("\n --- The number of total nodes is: ---\n");
-		Message("%d \t", count);
+		Message("%d \n", count);
 	}
 	free(elas_mode);
 }
