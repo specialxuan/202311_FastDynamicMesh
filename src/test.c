980b02c854f1babb50b4a25c43cf7279aaa47813
SpecialXuan <special.xuan@outlook.com>
2023-12-11 15:41:54 +0800

    update read_my_mode

--- src/test.c
+++ src/test.c
@@ -10,7 +10,44 @@
  */
 
 #include <math.h>
-#include "udf.h" // note: move udf.h to last
+#include "udf.h" // note: move udf.h to the last
+
+// define the read_mode()  to read input the array from the .txt file
+int read_my_mode(double *elas_mode, const int row, const int column)
+{
+#if !RP_NODE
+    FILE *fp = NULL;  // file pointer
+    int i = 0, j = 0; // indexes
+
+    fp = fopen("elas_mode.txt", "r"); // open the file in the read-only mode
+    if (fp == NULL)                   // file not exists -> print error
+    {
+        Message("\n ---        Error: No file.        ---\n");
+        return 1;
+    }
+
+    for (i = 0; i < row; i++) // fill the array
+        for (j = 0; j < column; j++)
+            if (fscanf(fp, "%lf", elas_mode + i * column + j) <= 0)
+            {
+                Message("\n ---   Error: Lack of variables.   ---\n From (%d,%d)", i, j);
+                fclose(fp);
+                return 1;
+            }
+
+    fclose(fp); // close file
+
+    Message("\n ---    The elas_mode array is:    ---\n"); // validation for the initialization of the elas_mode
+    for (j = 0; j < column; j++)                           // print variables
+        if (j == 0)
+            Message("%f \t", elas_mode[(row - 1) * column + j]);
+        else
+            Message("%f \t", elas_mode[(row - 1) * column + j]);
+    Message("\n ---      Validation is done!      ---\n");
+#endif
+
+    return 0;
+}
 
 static int iter_index = 1; // record the number of the iteration steps
 static int time_index = 0; // record the number of the time steps
@@ -26,8 +63,8 @@ DEFINE_ON_DEMAND(Preprocess)
     int row = 66654, column = 16;
     int n = 0, i = 0, count = 0; // record the number of total nodes
     double *elas_mode = NULL;    // elas_mode are the first 4 mode shapes array of the fluid mesh.
-                              // node-No.,x-coor,y-coor,z-coor,shape-information
-                              // 66654 row, 16column, Start at 0 when called
+                                 // node-No.,x-coor,y-coor,z-coor,shape-information
+                                 // 66654 row, 16column, Start at 0 when called
 
     iter_index = 1;
     time_index = 0;
@@ -39,6 +76,9 @@ DEFINE_ON_DEMAND(Preprocess)
 #if !RP_NODE
     elas_mode = (double *)malloc(row * column * sizeof(double)); // allocate memory for elas_mode
     memset(elas_mode, 0, row * column * sizeof(double));         // initialize elas_mode
+
+    if (read_my_mode(elas_mode, row, column))
+        Message("\n ---      Error: read_my_mode      ---\n");
 
     Message("iteration = %d, time = %d\n", iter_index, time_index);
     Message("hello world!\n");
