2b96504bf2b4246776ba113c400767374291fe27
SpecialXuan <special.xuan@outlook.com>
2023-12-12 14:38:39 +0800

    update count total number in each processor

--- src/test.c
+++ src/test.c
@@ -38,7 +38,7 @@ int read_my_mode(double *elas_mode, const int row, const int column)
     fclose(fp); // close file
 
     Message(" ---    The elas_mode array is:    ---\n"); // validation for the initialization of the elas_mode
-    for (j = 0; j < column; j++)                           // print variables
+    for (j = 0; j < column; j++)                         // print variables
         if (j == 0)
             Message("%f \t", elas_mode[(row - 1) * column + j]);
         else
@@ -56,12 +56,8 @@ DEFINE_ON_DEMAND(Preprocess)
 {
     FILE *fp = NULL;
 
-    Domain *domain;
-    cell_t c;
-    Thread *t;
-    Node *v;
     int row = 66654, column = 16;
-    int n = 0, i = 0, count = 0, fileStatus = 0; // record the number of total nodes
+    int n = 0, i = 0, nCount = 0, fileStatus = 0; // record the number of total nodes
     double *elas_mode = NULL;                    // elas_mode are the first 4 mode shapes array of the fluid mesh.
                                                  // node-No.,x-coor,y-coor,z-coor,shape-information
                                                  // 66654 row, 16column, Start at 0 when called
@@ -81,27 +77,67 @@ DEFINE_ON_DEMAND(Preprocess)
 
     if (fileStatus = read_my_mode(elas_mode, row, column))
         Message(" ---      Error: read_my_mode      ---\n");
-    
+
     Message(" ***      End:   This is Host      ***\n");
 #endif
 
     host_to_node_int_1(fileStatus);
     host_to_node_double(elas_mode, row * column);
 
-#if !RP_HOST
+#if !RP_HOST // TODO: add ! when compile
     Message("\n +++      Begin: This is Node      +++\n");
+
+    Domain *domain;
+    cell_t cell;
+    Thread *thread;
+    Node *node;
+    double *nCoor = (double *)malloc(row * 4 * sizeof(double));
+    memset(nCoor, 0, row * 4 * sizeof(double));
 
     domain = Get_Domain(1); // for single-phase flows, domain_id is 1 and Get_Domain(1) returns the fluid domain pointer
     if (fileStatus == 0)
     {
         Message(" ---    The elas_mode array is:    ---\n"); // validation for the initialization of the elas_mode
-        for (i = 0; i < column; i++)                           // print variables
+        for (i = 0; i < column; i++)                         // print variables
             if (i == 0)
                 Message("%f \t", elas_mode[(row - 1) * column + i]);
             else
                 Message("%f \t", elas_mode[(row - 1) * column + i]);
         Message(" ---      Validation is done!      ---\n");
     }
+    // NODE_X(node);
+
+    thread_loop_c(thread, domain)
+    {
+        begin_c_loop(cell, thread)
+        {
+            c_node_loop(cell, thread, n)
+            {
+                node = C_NODE(cell, thread, n);
+                // set the value of the first user define node memory as 0
+                // used to identify if the node has stored the shape information or not
+                N_UDMI(node, 12) = 0;
+            }
+        }
+        end_c_loop(cell, thread)
+    }
+    thread_loop_c(thread, domain)
+    {
+        begin_c_loop(cell, thread)
+        {
+            c_node_loop(cell, thread, n)
+            {
+                node = C_NODE(cell, thread, n);
+                if (N_UDMI(node, 12) == 0)
+                {
+                    N_UDMI(node, 12) = 1;
+                    nCount++;
+                }
+            }
+        }
+        end_c_loop(cell, thread)
+    }
+    Message("Total Number of nodes in NODE %d is %d\n", myid, nCount);
 
     Message(" +++      End:   This is Node      +++\n");
 #endif
