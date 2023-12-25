77773406c68a1ddb1d9eeb8efd328c62d07ad70a
SpecialXuan <special.xuan@outlook.com>
2023-12-25 16:52:54 +0800

    update

--- src/test.c
+++ src/test.c
@@ -135,79 +135,79 @@ DEFINE_ON_DEMAND(Preprocess)
         host_to_node_double(elas_mode, row * column);
     }
 
-    // #if !RP_HOST // TODO: add ! when compile
-    //     Message("\n +++      Begin: This is Node      +++\n");
+    #if !RP_HOST // TODO: add ! when compile
+        Message("\n +++      Begin: This is Node      +++\n");
 
-    //     // Domain *domain;
-    //     // cell_t cell;
-    //     // Thread *thread;
-    //     // Node *pNode;
+        Domain *domain;
+        cell_t cell;
+        Thread *thread;
+        Node *pNode;
 
-    //     // int n = 0, nCount = 0; // total number of node
-    //     // double *nCoor = (double *)malloc(row * 4 * sizeof(double));
-    //     // memset(nCoor, 0, row * 4 * sizeof(double));
+        int n = 0, nCount = 0; // total number of node
+        double *nCoor = (double *)malloc(row * 4 * sizeof(double));
+        memset(nCoor, 0, row * 4 * sizeof(double));
 
-    //     // char outFileName[20] = {0}; // output file name
-    //     // sprintf(outFileName, "outputNode%d.csv", myid);
-    //     // FILE *fpOutput = fopen(outFileName, "w+");
+        char outFileName[20] = {0}; // output file name
+        sprintf(outFileName, "outputNode%d.csv", myid);
+        FILE *fpOutput = fopen(outFileName, "w+");
 
-    //     // domain = Get_Domain(1); // for single-phase flows, domain_id is 1 and Get_Domain(1) returns the fluid domain pointer
-    //     // if (fileStatus == 0)
-    //     // {
-    //     //     Message(" ---    The elas_mode array is:    ---\n"); // validation for the initialization of the elas_mode
-    //     //     for (int i = 0; i < column; i++)                     // print variables
-    //     //         if (i == 0)
-    //     //             Message("%f \t", elas_mode[(row - 1) * column + i]);
-    //     //         else
-    //     //             Message("%f \t", elas_mode[(row - 1) * column + i]);
-    //     //     Message(" ---      Validation is done!      ---\n");
-    //     // }
+        domain = Get_Domain(1); // for single-phase flows, domain_id is 1 and Get_Domain(1) returns the fluid domain pointer
+        if (fileStatus == 0)
+        {
+            Message(" ---    The elas_mode array is:    ---\n"); // validation for the initialization of the elas_mode
+            for (int i = 0; i < column; i++)                     // print variables
+                if (i == 0)
+                    Message("%f \t", elas_mode[3 * column + i]);
+                else
+                    Message("%f \t", elas_mode[3 * column + i]);
+            Message(" ---      Validation is done!      ---\n");
+        }
 
-    //     // thread_loop_c(thread, domain)
-    //     // {
-    //     //     begin_c_loop(cell, thread)
-    //     //     {
-    //     //         c_node_loop(cell, thread, n)
-    //     //         {
-    //     //             pNode = C_NODE(cell, thread, n);
-    //     //             // set the value of the first user define node memory as 0
-    //     //             // used to identify if the node has stored the shape information or not
-    //     //             N_UDMI(pNode, 12) = 0;
-    //     //         }
-    //     //     }
-    //     //     end_c_loop(cell, thread)
-    //     // }
-    //     // thread_loop_c(thread, domain)
-    //     // {
-    //     //     begin_c_loop(cell, thread)
-    //     //     {
-    //     //         c_node_loop(cell, thread, n)
-    //     //         {
-    //     //             pNode = C_NODE(cell, thread, n);
-    //     //             if (N_UDMI(pNode, 12) == 0)
-    //     //             {
-    //     //                 nCoor[nCount * 4 + 0] = nCount + 1;
-    //     //                 nCoor[nCount * 4 + 1] = NODE_X(pNode);
-    //     //                 nCoor[nCount * 4 + 2] = NODE_Y(pNode);
-    //     //                 nCoor[nCount * 4 + 3] = NODE_Z(pNode);
+        thread_loop_c(thread, domain)
+        {
+            begin_c_loop(cell, thread)
+            {
+                c_node_loop(cell, thread, n)
+                {
+                    pNode = C_NODE(cell, thread, n);
+                    // set the value of the first user define node memory as 0
+                    // used to identify if the node has stored the shape information or not
+                    N_UDMI(pNode, 12) = 0;
+                }
+            }
+            end_c_loop(cell, thread)
+        }
+        thread_loop_c(thread, domain)
+        {
+            begin_c_loop(cell, thread)
+            {
+                c_node_loop(cell, thread, n)
+                {
+                    pNode = C_NODE(cell, thread, n);
+                    if (N_UDMI(pNode, 12) == 0)
+                    {
+                        nCoor[nCount * 4 + 0] = nCount + 1;
+                        nCoor[nCount * 4 + 1] = NODE_X(pNode);
+                        nCoor[nCount * 4 + 2] = NODE_Y(pNode);
+                        nCoor[nCount * 4 + 3] = NODE_Z(pNode);
 
-    //     //                 // fprintf(fpOutput, "%d,", nCount + 1);
-    //     //                 fprintf(fpOutput, "%f, %f, %f, %f,\n", nCoor[nCount * 4 + 0], nCoor[nCount * 4 + 1], nCoor[nCount * 4 + 2], nCoor[nCount * 4 + 3]);
-    //     //                 N_UDMI(pNode, 12) = 1;
-    //     //                 nCount++;
-    //     //                 // if (myid == 0)
-    //     //                 // {
-    //     //                 //     Message("%d\n", nCount);
-    //     //                 // }
-    //     //             }
+                        // fprintf(fpOutput, "%d,", nCount + 1);
+                        fprintf(fpOutput, "%f, %f, %f, %f,\n", nCoor[nCount * 4 + 0], nCoor[nCount * 4 + 1], nCoor[nCount * 4 + 2], nCoor[nCount * 4 + 3]);
+                        N_UDMI(pNode, 12) = 1;
+                        nCount++;
+                        // if (myid == 0)
+                        // {
+                        //     Message("%d\n", nCount);
+                        // }
+                    }
 
-    //     //         }
-    //     //     }
-    //     //     end_c_loop(cell, thread)
-    //     // }
-    //     // fclose(fpOutput);
-    //     // Message("Total Number of nodes in NODE %d is %d\n", myid, nCount);
+                }
+            }
+            end_c_loop(cell, thread)
+        }
+        fclose(fpOutput);
+        Message("Total Number of nodes in NODE %d is %d\n", myid, nCount);
 
-    //     Message(" +++      End:   This is Node      +++\n");
-    // #endif
+        Message(" +++      End:   This is Node      +++\n");
+    #endif
 }
